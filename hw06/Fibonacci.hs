83d292a73c510c368c3c8dcec7e652ec26eecc21
Eric Nething <emnething@gmail.com>
2015-07-24 01:33:38 -0400

    efficient fibonacci generator fibs2

--- hw06/Fibonacci.hs
+++ hw06/Fibonacci.hs
@@ -10,3 +10,6 @@ fib n = fib (n-1) + fib (n-2)
 fibs1 :: Integer -> [Integer]
 fibs1 n = take (fromIntegral n) $ foldr (\x acc -> fib x : acc) [] [0..]
 
+-- | Efficient fibonacci generator
+fibs2 :: [Integer]
+fibs2 = let go x1 x2 = x1 : go x2 (x1 + x2) in go 0 1
