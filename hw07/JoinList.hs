2ad1dcb84c2c19a27c51286d653b566ab959106d
Eric Nething <emnething@gmail.com>
2015-07-29 00:14:51 -0400

    added takeJ and testTakeJ

--- hw07/JoinList.hs
+++ hw07/JoinList.hs
@@ -52,15 +52,13 @@ indexJ n (Single _ a)
   | n == 0               = Just a                   -- found
   | otherwise            = Nothing                  -- not found
 indexJ n (Append m xs ys)
-  | n < 0 ||
-    n > getSize (size m) = Nothing                  -- out of bounds
   | n < leftSize         = indexJ n xs              -- go left
   | otherwise            = indexJ (n - leftSize) ys -- go right
 
   where leftSize = getSize . size . tag $ xs
 
 -- | Test indexJ
-testIndexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Bool
+testIndexJ :: (Sized b, Monoid b, Eq a) => Int -> JoinList b a -> Bool
 testIndexJ n xs = indexJ n xs == jlToList xs !!? n
 
 -- | Drop the specified number of elements from the front of the
@@ -69,13 +67,28 @@ dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
 dropJ n xs | n <= 0  = xs
 dropJ _ Empty        = Empty
 dropJ _ (Single _ _) = Empty
-dropJ n (Append m xs ys)
+dropJ n (Append _ xs ys)
   | n < leftSize     = dropJ n xs +++ ys
   | otherwise        = dropJ (n - leftSize) ys
                        
   where leftSize = getSize . size . tag $ xs
 
 -- | Test dropJ
-testDropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Bool
+testDropJ :: (Sized b, Monoid b, Eq a) => Int -> JoinList b a -> Bool
 testDropJ n xs = jlToList (dropJ n xs) == drop n (jlToList xs)
 
+-- | Take the specified number of elements from the front of the
+-- JoinList
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ n xs | n <= 0    = Empty
+takeJ _ Empty          = Empty
+takeJ _ s@(Single _ _) = s
+takeJ n (Append _ xs ys)
+  | n > leftSize       = xs +++ (takeJ (n - leftSize) ys)
+  | otherwise          = takeJ n xs
+
+  where leftSize = getSize . size . tag $ xs
+
+-- | Test takeJ
+testTakeJ :: (Sized b, Monoid b, Eq a) => Int -> JoinList b a -> Bool
+testTakeJ n xs = jlToList (takeJ n xs) == take n (jlToList xs)
