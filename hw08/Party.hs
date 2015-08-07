0a15ea4a82b7e349645c7855ab3dce195658fc94
Eric Nething <emnething@gmail.com>
2015-08-07 19:11:28 -0400

    hw8 ex 3

--- hw08/Party.hs
+++ hw08/Party.hs
@@ -4,6 +4,8 @@ module Party where
 
 import Employee
 import Data.Monoid
+import Data.Tree
+import Data.List (foldl1')
 
 -- | Add an Employee to the GuestList and update the Fun score without
 -- performing any checks
@@ -20,3 +22,18 @@ moreFun x@(GL _ t1) y@(GL _ t2)
   | t1 >= t2  = x
   | otherwise = y
 
+-- | Fold a Rose Tree
+treeFold :: Monoid m => (a -> m) -> Tree a -> m
+treeFold f (Node a xs) = f a <> mconcat (fmap (treeFold f) xs)
+
+-- | Given a Boss and a list of pairs of GuestList with their boss,
+-- and GuesList without their boss, calculate a pair of the best
+-- GuestList with this Boss, and without this Boss
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel boss xs = (foldl1' moreFun withBoss, foldl1' moreFun withoutBoss)
+  where withBoss = map (glCons boss . snd) xs
+        withoutBoss = foldr' (\(a,b) acc -> glCons boss a : glCons boss b : acc) [] xs
+
+-- | Calculate the best GuestList given an Employee hierarchy
+-- maxFun :: Tree Employee -> GuestList
+-- maxFun = treeFold ((:[]) . nextLevel)
